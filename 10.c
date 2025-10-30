d1e28de05c2a5fc3fe6130d24a1ab55d2efe0971
Qtrung474 <biunon83@gmail.com>
2025-10-30 11:19:37 +0700

    Update 10.c

--- 10.c
+++ 10.c
@@ -1,30 +1,16 @@
 #include <stdio.h>
 
+
 int main() {
-    int num, temp, divisor, digit; // divisor( chu so ), digit ( so chia ) 
+    char s[100];
+    int i;
     printf("Enter any integer: ");
-    scanf("%d", &num);
-    if (num == 0) {
-        printf("0\n");
-        return 0;
+    scanf("%s", s);
+    printf("The digits are: ");
+    for ( i = 0; s[i] != '\0'; i++) {
+        if (s[i] == '-')
+            continue;
+        printf("%c ", s[i]);
     }
-    if (num < 0) {
-        printf("- ");
-        num = -num;
-    }
-    divisor = 1;
-    temp = num;
-    while (temp >= 10) {
-        divisor = divisor * 10;
-        temp = temp / 10;
-    }
-    temp = num;
-    while (divisor > 0) {
-        digit = temp / divisor;
-        printf("%d ", digit);
-        temp = temp % divisor;
-        divisor = divisor / 10;
-    }
-    printf("\n");
     return 0;
 }
